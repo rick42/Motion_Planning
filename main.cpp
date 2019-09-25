19290c483375963b69e291913716b19f2b49e0d0
Ricky Valdez <vldzrcky@gmail.com>
2019-09-24 21:08:47 -0400

    BFS complete

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include "triangle.h"
+#include <vector>
+#include <queue>
 
 Display *display_ptr;
 Screen *screen_ptr;
@@ -26,17 +29,23 @@ char *icon_name_string = "Icon for Example Window";
 
 XEvent report;
 
-GC gc, gc_yellow, gc_red, gc_grey;
+GC gc, gc_yellow, gc_red, gc_grey, gc_white;
 unsigned long valuemask = 0;
 XGCValues gc_values, gc_yellow_values, gc_red_values, gc_grey_values;
 Colormap color_map;
 XColor tmp_color1, tmp_color2;
 
 void createWindow(int argc, char **argv);
+void drawObstacles(std::vector<Triangle> obstacles);
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 	int qx,qy,px,py,rx,ry;
+	int startX, startY, startAngle, targetX, targetY, targetAngle;
+	std::vector<Triangle> obstacles;
 	FILE * fp;
+
+	int xOffset, yOffset;
 
 	if(argc != 2){
 		printf("Please pass input file as an arguement\n");
@@ -44,16 +53,138 @@ int main(int argc, char **argv){
 	}
 
 	fp = fopen (argv[1],"r");
-	fscanf (fp,"V (%d,%d) (%d,%d) (%d,%d)", &qx,&qy,&px,&py,&rx,&ry);
+	fscanf (fp,"V (%d,%d) (%d,%d) (%d,%d)\n", &px,&py,&qx,&qy,&rx,&ry);
+	fscanf (fp,"S (%d,%d) %d\n", &startX, &startY, &startAngle);
+	fscanf (fp,"T (%d,%d) %d", &targetX, &targetY, &targetAngle);
+	Vehicle V(px,py,qx,qy,rx,ry,startX,startY,startAngle);
+	Vehicle target(px,py,qx,qy,rx,ry,targetX,targetY,targetAngle);
+
+	while(fscanf(fp,"\nO (%d,%d) (%d,%d) (%d,%d)", &px,&py,&qx,&qy,&rx,&ry) != EOF)
+	{
+		obstacles.push_back(Triangle(px,py,qx,qy,rx,ry));
+		printf("obstacles[%d]: q = (%d,%d), p = (%d,%d), r = (%d,%d)\n",obstacles.size()-1,
+										px,py,qx,qy,rx,ry);
+	}
+	printf("Number of obstacles = %d\n", obstacles.size());
+
 	fclose (fp);
-	printf ("I have read: q = (%d,%d), p = (%d,%d), r = (%d,%d)\n",qx,qy,px,py,rx,ry);
+
+	
+	printf ("I have read: q = (%d,%d), p = (%d,%d), r = (%d,%d)\n",px,py,qx,qy,rx,ry);
+	printf("startX = %d, startY = %d, startAngle = %d\n", startX, startY, startAngle);
+	printf("targetX = %d, targetY = %d, targetAngle = %d\n", targetX, targetY, targetAngle);
 
 	createWindow(argc, argv);
+
+
+
+	// BFS START
+	struct Position
+	{
+		short i, j, k;
+		Position(int i, int j, int k){this->i = i;this->j = j;this->k = k;};
+	};
+	struct Vertex{bool visited = 0; short prevI, prevJ, prevK;};
+	std::vector<Vertex> shortestPath;
+	Vertex visited[100][100][36];
+	std::queue<Position> BFSqueue;
+	short startPosI = startX / 5;
+	short startPosJ = startY / 5;
+	short startPosK = startAngle%360 / 10;
+	short targetPosI = targetX / 5;
+	short targetPosJ = targetY / 5;
+	short targetPosK = targetAngle%360 / 10;
+
+	printf("startPosI = %d, startPosJ = %d, startPosK = %d\n", startPosI,startPosJ,startPosK);
+	printf("targetPosI = %d, targetPosJ = %d, targetPosK = %d\n",targetPosI,targetPosJ,targetPosK);
+
+	int found = 0;
+	Position currPos(startPosI,startPosJ,startPosK);
+	Position prevPos = currPos;
+	BFSqueue.push(currPos);
+	while(found == 0  &&  !BFSqueue.empty())
+	{
+		prevPos = currPos;
+		currPos = BFSqueue.front();
+		BFSqueue.pop();
+
+		if(visited[currPos.i][currPos.j][currPos.k].visited == 0)
+		{
+			visited[currPos.i][currPos.j][currPos.k].prevI = prevPos.i;
+			visited[currPos.i][currPos.j][currPos.k].prevJ = prevPos.j;
+			visited[currPos.i][currPos.j][currPos.k].prevK = prevPos.k;
+
+			if(currPos.i == targetPosI  &&  currPos.j == targetPosJ  &&  currPos.k == targetPosK)
+			{
+				found = 1;
+			}
+			else
+			{
+				visited[currPos.i][currPos.j][currPos.k].visited = 1;
+
+				if(currPos.i > 0)
+					BFSqueue.push(Position(currPos.i - 1,currPos.j,currPos.k));
+				if(currPos.i < 99)
+					BFSqueue.push(Position(currPos.i + 1,currPos.j,currPos.k));
+				if(currPos.j > 0)
+					BFSqueue.push(Position(currPos.i,currPos.j - 1,currPos.k));
+				if(currPos.j < 99)
+					BFSqueue.push(Position(currPos.i,currPos.j + 1,currPos.k));
+
+				BFSqueue.push(Position(currPos.i,currPos.j,(currPos.k-1+36) % 36));
+				BFSqueue.push(Position(currPos.i,currPos.j,(currPos.k+1+36) % 36));
+			}
+		}
+	}
+
+	//printf("target visited =  %d\n", );
+
+	printf("PATHFOUND STATUS = %d\n", found);
+	printf("currPosI = %d, currPosJ = %d, currPosK = %d\n", currPos.i, currPos.j, currPos.k);
+
+	// BFS END
+	
+
+	
+	do
+	{
+		XNextEvent( display_ptr, &report);
+	}while(report.type != Expose);
+
+	XFlush(display_ptr);
+	drawObstacles(obstacles);
+	XFlush(display_ptr);
+	XFillPolygon(display_ptr,win, gc_red, target.points, 3, 2,0);
+	for(int i = 0; i < 100; i++)
+	{
+		XFlush(display_ptr);
+		XFillPolygon(display_ptr,win, gc, V.points, 3, 2,0);
+		XDrawPoint(display_ptr, win, gc_red,  V.center.x, V.center.y);
+		XFlush(display_ptr);
+      	usleep(200000);
+      	XFillPolygon(display_ptr,win, gc_white, V.points, 3, 2,0);
+      	V.translate(0,5);
+      	XFlush(display_ptr);
+      	V.rotate(-10);
+	}
+
+	XDestroyWindow(display_ptr, win);
+    XCloseDisplay(display_ptr);	
 
 	return 0;
 }
 
-void createWindow(int argc, char **argv){
+void drawObstacles(std::vector<Triangle> obstacles)
+{
+	for(int i = 0; i < obstacles.size(); i++)
+	{
+		XFlush(display_ptr);
+		XFillPolygon(display_ptr,win, gc_grey, obstacles[i].points, 3, 2,0);
+	}
+}
+
+void createWindow(int argc, char **argv)
+{
 	// opening display: basic connection to X Server 
 	if( (display_ptr = XOpenDisplay(display_name)) == NULL )
 	{
@@ -74,8 +205,10 @@ void createWindow(int argc, char **argv){
 	// creating the window 
 	border_width = 10;
 	win_x = 0; win_y = 0;
-	win_width = display_width/2;
-	win_height = (int) (win_width / 1.7); //rectangular window
+	//win_width = display_width/2;
+	//win_height = (int) (win_width / 1.7); //rectangular window
+	win_width = 500;
+	win_height = 500;
   
 	win= XCreateSimpleWindow( display_ptr, RootWindow( display_ptr, screen_num),
                               win_x, win_y, win_width, win_height, border_width,
@@ -110,12 +243,47 @@ void createWindow(int argc, char **argv){
 
 	// what events do we want to receive
 	XSelectInput( display_ptr, win, 
-            	  ExposureMask | StructureNotifyMask | ButtonPressMask );
+            	  ExposureMask);// | StructureNotifyMask | ButtonPressMask );
   
 	// finally: put window on screen 
 	XMapWindow( display_ptr, win );
 
 	XFlush(display_ptr);
-	sleep(10);
+
+	// create graphics context, so that we may draw in this window
+	gc = XCreateGC( display_ptr, win, valuemask, &gc_values);
+	XSetForeground( display_ptr, gc, BlackPixel( display_ptr, screen_num ) );
+	XSetLineAttributes( display_ptr, gc, 4, LineSolid, CapRound, JoinRound);
+
+	gc_white = XCreateGC( display_ptr, win, valuemask, &gc_values);
+	XSetForeground( display_ptr, gc_white, WhitePixel( display_ptr, screen_num ) );
+	XSetLineAttributes( display_ptr, gc_white, 4, LineSolid, CapRound, JoinRound);
+
+	// and three other graphics contexts, to draw in yellow and red and grey
+ 	gc_yellow = XCreateGC( display_ptr, win, valuemask, &gc_yellow_values);
+	XSetLineAttributes(display_ptr, gc_yellow, 6, LineSolid,CapRound, JoinRound);
+	if( XAllocNamedColor(display_ptr, color_map, "yellow", &tmp_color1, &tmp_color2 ) == 0 )
+	{
+    	printf("failed to get color yellow\n"); exit(-1);
+	}
+	else
+		XSetForeground( display_ptr, gc_yellow, tmp_color1.pixel );
+
+	gc_red = XCreateGC( display_ptr, win, valuemask, &gc_red_values);
+	XSetLineAttributes( display_ptr, gc_red, 6, LineSolid, CapRound, JoinRound);
+	if( XAllocNamedColor( display_ptr, color_map, "green", &tmp_color1, &tmp_color2 ) == 0 )
+	{
+    	printf("failed to get color red\n"); exit(-1);
+	}
+	else
+    	XSetForeground( display_ptr, gc_red, tmp_color1.pixel );
+
+	gc_grey = XCreateGC( display_ptr, win, valuemask, &gc_grey_values);
+	if( XAllocNamedColor( display_ptr, color_map, "light grey", &tmp_color1, &tmp_color2 ) == 0 )
+	{
+    	printf("failed to get color grey\n"); exit(-1);
+	}
+	else
+    	XSetForeground( display_ptr, gc_grey, tmp_color1.pixel );
 }
 
---
+++ triangle.h
@@ -0,0 +1,94 @@
+#include <math.h>
+
+#define PI 3.14159265
+
+struct Triangle
+{
+ 	Triangle(int, int, int, int, int, int);
+ 	XPoint points[3];
+};
+
+Triangle::Triangle (int px, int py, int qx, int qy, int rx, int ry)
+{
+ 	points[0].x = px;
+ 	points[0].y = py;
+ 	points[1].x = qx;
+ 	points[1].y = qy;
+ 	points[2].x = rx;
+ 	points[2].y = ry;
+}
+
+/*******************************************************/
+
+struct Vehicle : Triangle
+{
+	Vehicle(int, int, int, int, int, int, int, int, int);
+ 	XPoint center;
+ 	int angle = 0;
+	XPoint initialPoints[3];
+	void translate(int, int);
+	void rotate(int);
+};
+
+Vehicle::Vehicle(int px, int py, int qx, int qy, int rx, int ry,
+				 int startX, int startY, int startAngle) : Triangle(px, py, qx, qy, rx, ry)
+{
+	for(int i = 0; i < 3; i++)
+	{
+		initialPoints[i] = points[i];
+	}
+
+	center.x = 0;
+	center.y = 0;
+
+	translate(startX,startY);
+	rotate(startAngle);
+
+	printf("Before: %f     After: %d\n", 3.57, (int)round(3.57));
+}
+
+void Vehicle::translate(int xShift, int yShift)
+{
+	center.x += xShift;
+	center.y += yShift;
+
+	for (int i = 0; i < 3; i++)
+	{
+		points[i].x += xShift;
+		points[i].y += yShift;
+	}
+}
+
+void Vehicle::rotate(int angleShift)
+{
+	angle = (angle + angleShift);
+
+	if(angle >= 360  ||  angle < 0)
+		angle = angle % 360;
+
+	double radian = angle * (PI / 180.0);
+
+	if(angle == 0)
+	{
+		for(int i = 0; i < 3; i++)
+		{
+			points[i].x = initialPoints[i].x;
+			points[i].y = initialPoints[i].y;
+		}
+	}
+	else
+	{
+		for(int i = 0; i < 3; i++)
+		{
+			points[i].x = (int)round(initialPoints[i].x * cos(radian) - initialPoints[i].y * sin(radian));
+			points[i].y = (int)round(initialPoints[i].x * sin(radian) + initialPoints[i].y * cos(radian));
+		}
+	}
+
+	for(int i = 0; i < 3; i++)
+	{
+		points[i].x += center.x;
+		points[i].y += center.y;
+	}
+}
+
